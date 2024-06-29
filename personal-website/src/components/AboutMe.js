8e7466fc27ce95a75c44e8eb7be6e1faada7e855
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2024-06-29 15:31:48 -0400

    Add moving border animation to text at bottom of home page

--- personal-website/src/components/AboutMe.js
+++ personal-website/src/components/AboutMe.js
@@ -1,19 +1,24 @@
 import './AboutMe.css';
+import './MovingBorderBox.css'
 
 function AboutMe() {
     return (
-        <div class="home-bottom">
-            <p class="p1 chivo">
-                I'm currently a CS student at <span class="yellow">Georgia Tech</span> interested in <span class="orange">cloud development</span> and <span class="orange">automation.</span>
-            </p>
-            <p class="dots">···</p>
-            <p class="p2 chivo">
-                In my free time, I love <span class="yellow">reading</span> fantasy and thriller books, watching <span class="yellow">F1 races</span>, and learning about anything <span class="yellow">ancient history</span> related.
-            </p>
-            <p class="dots">···</p>
-            <p class="p3 chivo">
-                I thrive in collaborative environments and enjoy the challenge that comes with learning new skills.
-            </p>
+        <div className="home-bottom">
+            <div className="border box">
+                <div className="paragraph-container">
+                    <p className="p1 chivo">
+                        I'm currently a CS student at <span class="yellow">Georgia Tech</span> interested in <span class="orange">cloud development</span> and <span class="orange">data engineering.</span>
+                    </p>
+                    <p className="dots">···</p>
+                    <p className="p2 chivo">
+                        In my free time, I love <span class="yellow">reading</span> fantasy and thriller books, watching <span class="yellow">F1 races</span>, and learning about anything <span class="yellow">ancient history</span> related.
+                    </p>
+                    <p className="dots">···</p>
+                    <p className="p3 chivo">
+                        I thrive in collaborative environments and enjoy the challenge that comes with learning new skills.
+                    </p>
+                </div>
+            </div>
         </div>
     );
   }
