715c1a34eb1d60261b5a4bbcc8f437a99b34a2ff
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2024-06-30 15:21:03 -0400

    Add typing animation for home page

--- personal-website/src/components/TypingTextBox.js
+++ personal-website/src/components/TypingTextBox.js
@@ -1,10 +1,34 @@
 import './TypingTextBox.css'
+import { TypeAnimation } from 'react-type-animation';
+
+// function TypingTextBox() {
+//     return (
+//         <div class="typing-text">
+//             <p>Programmer</p>
+//         </div>
+//     );
+// }
+
+const TYPING_TEXT_CLASS = 'typing-text';
 
 function TypingTextBox() {
     return (
-        <div class="typing-text">
-            <p>Programmer</p>
-        </div>
+        <TypeAnimation
+            className={TYPING_TEXT_CLASS}
+            sequence={[
+                'Programmer',
+                1000, // Waits 1s
+                'F1 Enthusiast', 
+                1000, 
+                'Avid Reader',
+                1000,
+                'A Happy Guy :)',
+                1000,
+            ]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+        />
     );
 }
 
