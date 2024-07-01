1ccb3a881f853d919c2efdb41efd11de61b5d1f0
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2024-06-30 21:23:02 -0400

    Update all project cards

--- personal-website/src/pages/Projects.js
+++ personal-website/src/pages/Projects.js
@@ -3,6 +3,8 @@ import ProjectCard from "../components/ProjectCard";
 import ProjectCardNoDemo from "../components/ProjectCardNoDemo";
 import './Projects.css'
 import MemAssistImg from '../images/MemoryAssist.png'
+import RecipeImg from '../images/RecipeSuggestion.png'
+import SafePassImg from '../images/SafePass.png'
 
 export default function Projects() {
     return (
@@ -15,24 +17,24 @@ export default function Projects() {
                 </header>
                 <div className="cards-container">
                     <ProjectCard imgSrc={MemAssistImg} imgAlt={"Card Image"}
-                    title={"Memory Assist"}
+                    title={"Memory Assist - A New Way to Study"}
                     description={"An AI-powered study assistant built by training an LLM on personal DSA notes. It provides responses with both text and image references from the uploaded notes."}
                     button1Text={"Repo"} button2Text={"Demo"}
                     repoLink={"https://github.com/EL132/Memory-Assistant"} demoLink={"https://memory-assistant-97ft.vercel.app/"}
                     />
 
-                    <ProjectCardNoDemo imgSrc={MemAssistImg} imgAlt={"Card Image"}
-                    title={"Memory Assist"}
-                    description={"An AI-powered study assistant built by training an LLM on personal DSA notes. It provides responses with both text and image references from the uploaded notes."}
+                    <ProjectCardNoDemo imgSrc={RecipeImg} imgAlt={"Card Image"}
+                    title={"Recipe Suggestion With Computer Vision"}
+                    description={"Take and upload a picture of the available fruits you have in front of you. The model will analyze what's on the table and recommend tailored recipes."}
                     buttonText={"Repo"}
-                    repoLink={"https://github.com/EL132/Memory-Assistant"}
+                    repoLink={"https://github.com/FevinFelix/Recipe-Suggestions-with-Fruit-Object-Detection"}
                     />
 
-                    <ProjectCardNoDemo imgSrc={MemAssistImg} imgAlt={"Card Image"}
-                    title={"Memory Assist"}
-                    description={"An AI-powered study assistant built by training an LLM on personal DSA notes. It provides responses with both text and image references from the uploaded notes."}
+                    <ProjectCardNoDemo imgSrc={SafePassImg} imgAlt={"Card Image"}
+                    title={"Safe-Pass Password Manager"}
+                    description={"Local password manager with multi-layer authentication and advanced hashing functions. Retrieve, delete, and add passwords anytime with secure login credentials."}
                     buttonText={"Repo"}
-                    repoLink={"https://github.com/EL132/Memory-Assistant"}
+                    repoLink={"https://github.com/FevinFelix/Safe-Pass-Password-Manager"}
                     />
                 </div>
             </main>
